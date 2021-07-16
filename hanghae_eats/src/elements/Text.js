efe70f4a6d28ecf77d15855e7e6671a9a453ab2d
dahyunyang <dahyunyang.0824@gmail.com>
2021-07-17 06:26:42 +0900

    로그인 2차

--- hanghae_eats/src/elements/Text.js
+++ hanghae_eats/src/elements/Text.js
@@ -2,9 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 const Text = (props) => {
-    const {bold, color, size, children} = props;
+    const {bold, color, size, children, width, margin} = props;
 
-    const styles = {bold:bold, color:color, size:size};
+    const styles = {
+        bold:bold, color:color, size:size, width, margin
+    };
 
     return(
         <React.Fragment>
@@ -18,9 +20,13 @@ Text.defaultProps = {
     bold: false,
     color: "#222831",
     size: "15px",
+    width: "auto",
+    margin: "auto",
 };
 
 const P = styled.p`
+    width: ${(props) => props.width};
+    margin: ${(props) => props.margin};   
     color: ${(props) => props.color};
     font-size: ${(props) => props.size};
     font-weight: ${(props) => (props.bold? "600" : "400")};
--- hanghae_eats/src/pages/Login.js
+++ hanghae_eats/src/pages/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Grid, Input, Button, Text } from '../elements';
+import { Grid, Input, Button, Text, Image } from '../elements';
+import { history } from '../redux/configStore';
 import Footer from '../shared/Footer';
 
 const Login = (props) => {
@@ -7,25 +8,35 @@ const Login = (props) => {
     <React.Fragment>
       <Grid padding ="16px" align="center" width="50%">
         <Grid>
-          로고 이미지
+          <Text font-size="50px">여기 로고</Text>
+        </Grid>
+
+        <Grid is_flex padding="16px 0px">
+          <Grid width="10%">
+            <Text>아이디</Text>
+          </Grid>
+          <Grid width="90%">
+            <Input pading="6px" placeholder="아이디(이메일)입력"/>
+          </Grid>
+        </Grid>
+
+        <Grid is_flex padding="16px 0px">
+          <Grid width="10%">
+            <Text>비밀번호</Text>
+          </Grid>
+          <Grid width="90%">
+            <Input pading="6px" placeholder="비밀번호 입력"/>
+          </Grid>
         </Grid>
 
         <Grid padding="16px 0px">
-          <Input pading="6px" placeholder="아이디(이메일)입력"/>
-        </Grid>
-
-        <Grid padding="16px 0px">
-          <Input pading="6px" placeholder="비밀번호 입력"/>
-        </Grid>
-
-        <Grid padding="16px 0px">
-          <Button width = "90%" height="40px">
+          <Button width = "100%" height="40px">
             <Text bold color="#ffffff">로그인</Text>
           </Button>
         </Grid>
 
         <Grid padding="16px 0px">
-          <Text size="13px" color="#50A0FF">회원가입</Text>
+          <Text size="13px" color="#50A0FF" onClick={() => props.history.push("/register")}>회원가입</Text>
         </Grid>
         <Grid padding="16px 0px">
           <Footer/>
