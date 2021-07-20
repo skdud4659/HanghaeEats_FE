a7f9d54c316c1809e11110f65d4e4bfd9fd56899
skdud4659 <skdud4659@gmail.com>
2021-07-21 03:44:40 +0900

    즐겨찾기 페이지 이동하게 수정, 별점 수정

--- hanghae_eats/src/components/FavoriteList.js
+++ hanghae_eats/src/components/FavoriteList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Grid, Text, Image} from '../elements';
 import {useSelector} from 'react-redux';
+import {history} from '../redux/configStore';
 
 //이모지
 import { FaStar} from "react-icons/fa";
@@ -9,20 +10,26 @@ const FavoriteList = (props) => {
   const store_list = useSelector((state) => state.stores.stores)
   const store_idx = store_list.findIndex((s) => s._id === props.f)
   const store_info = store_list[store_idx]
+  console.log(store_info)
 
   // 별점 소수점 첫째자리까지
-  const rateStar = Number(store_info.avgStar).toFixed(1)
+  const rateStar = store_info.avgStar==="0" ? 0 : Number(store_info.avgStar).toFixed(1)
+
+  //각 매장으로 이동
+  const storeBtn = () => {
+    history.push(`/storeDetail/${props.f}`)
+  }
 
   return (
     <React.Fragment>
-      <Grid is_flex padding="2%">
+      <Grid is_flex padding="2%" _onClick={storeBtn}>
         {/* 매장 이미지 */}
         <Image width="110%" height="150px"src={store_info.image}/>
         {/* 매장 이름, 별점(후기 수) */}
         <Grid margin="0px 3%">
           <Text bold size="25px" margin="1% 0px">{store_info.name}</Text>
           <Grid is_flex margin="1% 0px">
-            <FaStar color={'#f7d57f'} size="4%"/>
+            <FaStar color={'#f7d57f'} size="30px"/>
             <Text margin="0px 1%">{rateStar}({store_info.orders})</Text>
             <Grid></Grid>
           </Grid>
