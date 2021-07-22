33387539774bc971bc8492373ac5baddd5645dff
skdud4659 <skdud4659@gmail.com>
2021-07-22 23:20:01 +0900

    Update Menu.js

--- hanghae_eats/src/components/Menu.js
+++ hanghae_eats/src/components/Menu.js
@@ -29,18 +29,19 @@ const Menu = (props) => {
   //수량에 따른 금액 계산
   const countPrice = price*count
 
+  const is_login = useSelector((state) => state.user.is_login)
+
   //카트에 담기 + 다른 매장인 경우 주문 불가
   const addCartBtn = () => {
     //카트가 비었을 경우 그냥 담기
-    console.log(cart_list)
     if (cart_list.length === 0) {
-      dispatch(addCart({_id, storeId, name, countPrice, count, price}))
+      is_login ? dispatch(addCart({_id, storeId, name, countPrice, count, price})) : window.alert('로그인 상태에만 주문이 가능해요!')
     } else {
        //카트에 리스트가 있을 경우 아이디값 대조하여 return
       if(storeId !== cart_list[0].storeId) {
         window.alert('동일한 매장에서만 주문이 가능해요!')
       } else {
-        dispatch(addCart({_id, storeId, name, countPrice, count, price}))
+        is_login ? dispatch(addCart({_id, storeId, name, countPrice, count, price})) : window.alert('로그인 상태에서만 주문이 가능해요!')
       }
     }
   }
